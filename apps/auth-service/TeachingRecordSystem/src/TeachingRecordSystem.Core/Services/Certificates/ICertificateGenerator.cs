4be2ae42c854452c7d5677a4e38cfe96008adefa
Brad Park (Igi) <brad@igi.codes>
2024-07-05 13:10:10 +0100

    SWECFD-514 - Remove Certificates services (DFE-Digital/csc-social-work-ecf-digital-auth#12)
    * SWECFD-512 - Remove Establishments service
    
    * SWECFD-513 - Remove DataverseAdapter
    
    * SWECFD-514 - Remove Certificates services

--- apps/auth-service/TeachingRecordSystem/src/TeachingRecordSystem.Core/Services/Certificates/ICertificateGenerator.cs
+++
@@ -1,6 +0,0 @@
-namespace TeachingRecordSystem.Core.Services.Certificates;
-
-public interface ICertificateGenerator
-{
-    Task<Stream> GenerateCertificate(string templateName, IReadOnlyDictionary<string, string> fieldValues);
-}
