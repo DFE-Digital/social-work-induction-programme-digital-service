989cb538b395883df7e42f35a0ce8f6207ce98a3
varshakrishna-dfe <168825421+varshakrishna-dfe@users.noreply.github.com>
2024-08-05 17:05:00 +0100

    Negative Tests (DFE-Digital/csc-social-work-ecf-digital#34)
    Co-authored-by: varshakrishna-hippo <166136888+varshakrishna-hippo@users.noreply.github.com>

--- apps/user-management/apps/swe-api-mimic-test/DfeSwwEcf.SweApiSimulator.Tests/FunctionalTests/socialWorker.test.ts
+++ apps/user-management/apps/swe-api-mimic-test/DfeSwwEcf.SweApiSimulator.Tests/FunctionalTests/socialWorker.test.ts
@@ -5,6 +5,8 @@ import { describe, it } from '@serenity-js/playwright-test';
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 describe('GET Social Worker Details', () => {
+
+    // Positive Test Case: Valid SWID
     it('should return the social worker details with status 200', async ({ actorCalled }) => {
         const actor = actorCalled('Alice');
         await actor.attemptsTo(
@@ -31,6 +33,75 @@ describe('GET Social Worker Details', () => {
                 ))
         );
     });
-});
 
+    // Test Case 3: Negative Test - SWE ref (Unmatched SWID)
+    it('should return 200 OK with invalid request message for unmatched SWID', async ({ actorCalled }) => {
+        const actor = actorCalled('Alice');
+        await actor.attemptsTo(
+            Send.a(GetRequest.to('/SocialWorker/95')), // A
+            // Ensure the response status code is 200
+            Ensure.that(LastResponse.status(), equals(200)),
+            // Ensure the response body contains the expected message
+            Ensure.that(LastResponse.body(),
+                equals('Invalid Request')
+            )
+        );
+    });
+
+    // Test Case 4: Negative Test - SWE ref (Null SWID)
+    it('should return 400 Bad Request for null SWID', async ({ actorCalled }) => {
+        const actor = actorCalled('Alice');
+        await actor.attemptsTo(
+            Send.a(GetRequest.to('/SocialWorker/')),
+            // Ensure the response status code is 400
+            Ensure.that(LastResponse.status(), equals(400)),
+            // Ensure the response body contains the expected error message
+            Ensure.that(LastResponse.body(),
+                equals('Please provide non-null value')
+            )
+        );
+    });
+
+    // Test Case 5: Negative Test - SWE ref (SWID Greater Than Max Char)
+    it('should return 500 Internal Server Error for too large SWID', async ({ actorCalled }) => {
+        const actor = actorCalled('Alice');
+        await actor.attemptsTo(
+            Send.a(GetRequest.to('/SocialWorker/111222333444555666')), // Exceeds Int32 max size
+            // Ensure the response status code is 500
+            Ensure.that(LastResponse.status(), equals(500)),
+            // Ensure the response body contains the expected error message
+            Ensure.that(LastResponse.body(),
+                equals('Internal server error: One or more error occurred. (Value was either too large or too small for an Int32.)')
+            )
+        );
+    });
+
+    // Test Case 6: Negative Test - SWE ref (Invalid SWID Format Non Int)
+    it('should return 422 Unprocessable Entity for invalid SWID format', async ({ actorCalled }) => {
+        const actor = actorCalled('Alice');
+        await actor.attemptsTo(
+            Send.a(GetRequest.to('/SocialWorker/SW')),
+            // Ensure the response status code is 422
+            Ensure.that(LastResponse.status(), equals(422)),
+            // Ensure the response body contains the expected error message
+            Ensure.that(LastResponse.body(),
+                property('error', equals('Please enter valid integer value'))
+            )
+        );
+    });
+
+    // Test Case 7: Negative Test - SWE Number (Value with in range but Non-existent SWID)
+    it('should return 404 Not Found for non-existent SWID', async ({ actorCalled }) => {
+        const actor = actorCalled('Alice');
+        await actor.attemptsTo(
+            Send.a(GetRequest.to('/SocialWorker/99999999')),
+            // Ensure the response status code is 404
+            Ensure.that(LastResponse.status(), equals(404)),
+            // Ensure the response body contains the expected error message
+            Ensure.that(LastResponse.body(),
+                equals('No SocialWorker found with this ID')
+            )
+        );
+    });
+});
 
