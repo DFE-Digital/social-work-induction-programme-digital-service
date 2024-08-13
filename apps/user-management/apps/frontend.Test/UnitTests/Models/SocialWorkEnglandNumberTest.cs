0be5414bd772a5c65c2c08856b8ae52d6cd53694
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2024-08-13 15:02:13 +0100

    Swecfd 709 swe type (DFE-Digital/csc-social-work-ecf-digital#36)
    * SWE type and unit tests
    
    * Remove unused directives
    
    * Refactor the class, include validator and update tests
    
    * Fix unit test
    
    * Refactor as type
    
    * Tidy up
    
    * Remove redundant initialisation
    
    * Remove unused directives
    
    * Add constructor, change parse, improve readability
    
    * Cater for defaults

---
+++ apps/user-management/apps/frontend.Test/UnitTests/Models/SocialWorkEnglandNumberTest.cs
@@ -0,0 +1,113 @@
+﻿using Dfe.Sww.Ecf.Frontend.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Dfe.Sww.Ecf.Frontend.Test.UnitTests.Models;
+
+public class SocialWorkEnglandNumberTest
+{
+    private readonly DateOnly _today = DateOnly.FromDateTime(DateTime.Now);
+
+    [Fact]
+    public void WhenSweNumberNegativeSingleParam_ThrowException()
+    {
+        //Arrange & Act
+        Action action = () => _ = new SocialWorkEnglandNumber(-1);
+
+        //Assert
+        action.Should().Throw<ArgumentException>().WithMessage("Social Work England number must not be negative");
+    }
+
+    [Fact]
+    public void WhenSweNumberNegative_ThrowException()
+    {
+        //Arrange & Act
+        Action action = () => _ = new SocialWorkEnglandNumber(-1,_today);
+
+        //Assert
+        action.Should().Throw<ArgumentException>().WithMessage("Social Work England number must not be negative");
+    }
+
+    [Fact]
+    public void WhenDueDateInFuture_RegisteredCheckRequireFalse()
+    {
+        //Arrange
+        var account = new SocialWorkEnglandNumber(new Random().Next(), _today.AddDays(1));
+
+        //Act
+        var result = account.IsStatusCheckRequired();
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WhenCheckIsDue_RegisteredCheckRequireTrue()
+    {
+        //Arrange
+        var account = new SocialWorkEnglandNumber(new Random().Next(), _today);
+
+        //Act
+        var result = account.IsStatusCheckRequired();
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("SW1234")]
+    [InlineData("1234")]
+    public void ExtractNumber_ReturnsInt(string sweId)
+    {
+        //Arrange
+        var expectedObject = new SocialWorkEnglandNumber(1234);
+
+        //Act
+        var result = SocialWorkEnglandNumber.Parse(sweId);
+
+        //Assert
+        result.Should().BeEquivalentTo(expectedObject);
+    }
+
+    [Fact]
+    public void WhenArgumentIsNull_EqualsReturnsFalse()
+    {
+        //Arrange
+        var account = new SocialWorkEnglandNumber(new Random().Next(), _today);
+
+        //Act
+        var result = account.Equals(null);
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WhenObjectIsNotSocialWorkEnglandNumber_EqualsReturnsFalse()
+    {
+        //Arrange
+        var account = new SocialWorkEnglandNumber(new Random().Next(), _today);
+        var obj = new object();
+
+        //Act
+        var result = account.Equals(obj);
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WhenObjectsHoldTheSameNumber_EqualsReturnsTrue()
+    {
+        //Arrange
+        var account1 = new SocialWorkEnglandNumber(1234, _today);
+        var account2 = new SocialWorkEnglandNumber(1234, _today.AddDays(1));
+
+        //Act
+        var result = account1.Equals(account2);
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+}
---
+++ apps/user-management/apps/frontend/Models/SocialWorkEnglandNumber.cs
@@ -0,0 +1,86 @@
+﻿namespace Dfe.Sww.Ecf.Frontend.Models;
+
+public sealed class SocialWorkEnglandNumber
+{
+    private readonly int _socialWorkEnglandNumber;
+
+    private readonly DateOnly _statusCheckDueDate;
+
+    public SocialWorkEnglandNumber(int socialWorkEnglandNumber)
+    {
+        if (socialWorkEnglandNumber < 0)
+        {
+            throw new ArgumentException("Social Work England number must not be negative");
+        }
+        _socialWorkEnglandNumber = socialWorkEnglandNumber;
+        _statusCheckDueDate = default;
+    }
+
+    public SocialWorkEnglandNumber(int socialWorkEnglandNumber, DateOnly statusCheckDueDate)
+    {
+        if (socialWorkEnglandNumber < 0)
+        {
+            throw new ArgumentException("Social Work England number must not be negative");
+        }
+        _socialWorkEnglandNumber = socialWorkEnglandNumber;
+        _statusCheckDueDate = statusCheckDueDate;
+    }
+
+    public int GetNumber()
+    {
+        return _socialWorkEnglandNumber;
+    }
+
+    /// <summary>
+    /// Get new instance with updated due date
+    /// based on today's date incremented by provided number of days
+    /// </summary>
+    /// <param name="incrementDays"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public SocialWorkEnglandNumber UpdateDueDate(int incrementDays)
+    {
+        if (incrementDays > -1)
+        {
+            return new SocialWorkEnglandNumber(_socialWorkEnglandNumber, DateOnly.FromDateTime(DateTime.Now).AddDays(incrementDays));
+        }
+        throw new ArgumentException("Increment value must not be negative");
+    }
+
+    /// <summary>
+    /// Return Social Work England Number object from string identifier
+    /// </summary>
+    /// <param name="sweId"></param>
+    /// <returns></returns>
+    public static SocialWorkEnglandNumber Parse(string sweId)
+    {
+        var sweNumber = sweId.StartsWith("SW") ? Int32.Parse(sweId.Remove(0, 2)) : Int32.Parse(sweId);
+        return new SocialWorkEnglandNumber(sweNumber);
+    }
+
+    /// <summary>
+    /// Determine if Social Work England registration status check is due
+    /// </summary>
+    /// <returns>boolean for whether a check is needed</returns>
+    public bool IsStatusCheckRequired()
+    {
+        return _statusCheckDueDate <= DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    public override int GetHashCode()
+    {
+        return _socialWorkEnglandNumber;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj is not SocialWorkEnglandNumber account)
+            return false;
+        return _socialWorkEnglandNumber == account.GetNumber();
+    }
+
+    public override string ToString()
+    {
+        return $"{_socialWorkEnglandNumber.ToString()}";
+    }
+}
