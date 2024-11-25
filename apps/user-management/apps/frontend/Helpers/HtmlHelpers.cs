49274b3cd8aa80734e5d4d7d1b628dd9c9515bd7
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2024-11-25 11:23:44 +0000

    [SWECFD-823] Highlight active tab (DFE-Digital/csc-social-work-ecf-digital#82)
    * highlight active tab
    
    * edit logic so tab is active for child routes

---
+++ apps/user-management/apps/frontend/Helpers/HtmlHelpers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dfe.Sww.Ecf.Frontend.Helpers;
+
+public static class HtmlHelpers
+{
+    public static string IsActive(
+        this IHtmlHelper html,
+        string page,
+        string cssClass = "dfe-header__navigation-item--current"
+    )
+    {
+        var currentPage = html.ViewContext.RouteData.Values["page"]?.ToString();
+        return currentPage?.StartsWith(page, StringComparison.OrdinalIgnoreCase) ?? false
+            ? cssClass
+            : string.Empty;
+    }
+}
