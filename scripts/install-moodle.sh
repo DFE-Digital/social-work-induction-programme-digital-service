8078481aadd05e4f16388f1a7e9dd12db99a429d
Brad Park (Igi) <brad@igi.codes>
2025-02-11 10:00:52 +0000

    SWIP-234 - Map local config.php file as docker volume to pre-select govuk theme (#8)

--- scripts/install-moodle.sh
+++ scripts/install-moodle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it --user www-data moodle-web php /var/www/html/admin/cli/install.php --non-interactive --agree-license --wwwroot=http://localhost:8080 --dbtype=pgsql --dbhost=postgres --dbport=5432 --dbname=moodle --dbuser=moodle_user --dbpass=moodle_password --fullname="Moodle Demo" --shortname=Demo --adminpass=password
+docker exec -it --user www-data moodle-web php /var/www/html/admin/cli/install_database.php --agree-license --fullname="Social Work Induction Programme" --shortname=SWIP --adminpass=password --adminemail=admin@email.com
