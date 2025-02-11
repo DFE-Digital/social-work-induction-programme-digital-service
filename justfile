5994740f9b40f42a5ef7cfe081b368043eb052a5
Brad Park (Igi) <brad@igi.codes>
2025-02-11 10:39:42 +0000

    SWIP-247 - Use env vars in compose/configs

--- justfile
+++ justfile
@@ -11,4 +11,4 @@ clean:
   docker compose down -v
 
 install-moodle:
-  . ./scripts/install-moodle.sh
+  docker exec --env-file development.env -it --user www-data moodle-web bash -c 'php /var/www/html/admin/cli/install_database.php --agree-license --fullname="${MOODLE_SITE_FULLNAME}" --shortname="${MOODLE_SITE_SHORTNAME}" --adminpass="${MOODLE_ADMIN_PASSWORD}" --adminemail="${MOODLE_ADMIN_EMAIL}"'
--- moodle-docker/config.php
+++ moodle-docker/config.php
@@ -4,22 +4,39 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'pgsql';
+$CFG->dbtype = getenv('MOODLE_DB_TYPE');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'postgres';
-$CFG->dbname    = 'moodle';
-$CFG->dbuser    = 'moodle_user';
-$CFG->dbpass    = 'moodle_password';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
+$CFG->dbhost = getenv('MOODLE_DB_HOST');
+$CFG->dbname = getenv('POSTGRES_DB');
+$CFG->dbuser = getenv('POSTGRES_USER');
+$CFG->dbpass = getenv('POSTGRES_PASSWORD');
+$CFG->prefix = getenv('MOODLE_DB_PREFIX');
+$CFG->dboptions = [
   'dbpersist' => 0,
   'dbport' => 5432,
   'dbsocket' => '',
-);
+];
 
-$CFG->wwwroot   = 'http://localhost:8080';
-$CFG->dataroot  = '/var/www/moodledata';
-$CFG->admin     = 'admin';
+if (empty($_SERVER['HTTP_HOST'])) {
+  $_SERVER['HTTP_HOST'] = 'localhost';
+}
+$host = 'localhost';
+if (!empty(getenv('MOODLE_DOCKER_WEB_HOST'))) {
+  $host = getenv('MOODLE_DOCKER_WEB_HOST');
+}
+$CFG->wwwroot = "http://{$host}";
+$port = getenv('MOODLE_DOCKER_WEB_PORT');
+if (!empty($port)) {
+  // Extract port in case the format is bind_ip:port.
+  $parts = explode(':', $port);
+  $port = end($parts);
+  if ((string) (int) $port === (string) $port) { // Only if it's int value.
+    $CFG->wwwroot .= ":{$port}";
+  }
+}
+
+$CFG->dataroot = '/var/www/moodledata';
+$CFG->admin = getenv('MOODLE_ADMIN_USER');
 
 $CFG->directorypermissions = 02777;
 
