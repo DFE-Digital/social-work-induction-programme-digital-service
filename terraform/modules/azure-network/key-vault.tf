62e27fa943a4596ee1b689c74f0557cc044c8aa9
boblaw-dfe <armand.lawson@education.gov.uk>
2025-02-20 16:25:54 +0000

    SWIP-207 - Remove kv data protection

--- terraform/modules/azure-network/key-vault.tf
+++ terraform/modules/azure-network/key-vault.tf
@@ -62,19 +62,3 @@ resource "azurerm_key_vault_access_policy" "kv_gh_ap" {
     ignore_changes = [object_id]
   }
 }
-
-resource "azurerm_key_vault_key" "data-protection" {
-  name         = "data-protection"
-  key_vault_id = azurerm_key_vault.kv.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-}
