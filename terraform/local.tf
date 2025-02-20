47d03dae7a8464f05f4dd4c2fb29f99364c1c1c6
boblaw-dfe <armand.lawson@education.gov.uk>
2025-02-20 10:38:00 +0000

    Initial terraform - removed unused env vars

--- terraform/local.tf
+++ terraform/local.tf
@@ -14,13 +14,6 @@ locals {
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
     "KeyVault__Endpoint"                  = "https://${var.resource_name_prefix}-kv.vault.azure.net/"
     "WEBSITES_PORT"                       = "8080"
-    "ServiceAccess__IsPublic"             = var.webapp_access_is_public
-    "ServiceAccess__Keys__0"              = var.webapp_e2e_access_key
-    "ServiceAccess__Keys__1"              = var.webapp_team_access_key
-    "ServiceAccess__Keys__2"              = var.webapp_access_key_1
-    "ServiceAccess__Keys__3"              = var.webapp_access_key_2
-    "GTM__Tag"                            = var.gtm_tag
-    "Clarity__Tag"                        = var.clarity_tag
   }
 
   webapp_slot_app_settings = {
@@ -29,10 +22,5 @@ locals {
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
     "KeyVault__Endpoint"                  = "https://${var.resource_name_prefix}-kv.vault.azure.net/"
     "WEBSITES_PORT"                       = "8080"
-    "ServiceAccess__IsPublic"             = var.webapp_access_is_public
-    "ServiceAccess__Keys__0"              = var.webapp_e2e_access_key
-    "ServiceAccess__Keys__1"              = var.webapp_team_access_key
-    "ServiceAccess__Keys__2"              = var.webapp_access_key_1
-    "ServiceAccess__Keys__3"              = var.webapp_access_key_2
   }
 }
