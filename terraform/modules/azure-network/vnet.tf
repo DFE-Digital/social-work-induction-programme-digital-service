142b13f38de18c16466fec0d524ec7fe39d4bcfb
boblaw-dfe <armand.lawson@education.gov.uk>
2025-02-24 16:20:45 +0000

    SWIP-207 - increase vnet size

--- terraform/modules/azure-network/vnet.tf
+++ terraform/modules/azure-network/vnet.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.resource_name_prefix}-vnet"
   location            = var.location
   resource_group_name = var.resource_group
-  address_space       = ["10.0.0.0/26"]
+  address_space       = ["10.0.0.0/22"]
 
   lifecycle {
     ignore_changes = [
