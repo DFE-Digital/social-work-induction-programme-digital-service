248ec2b46db07f7bb864d8f77ddb8afb38ad7629
boblaw-dfe <armand.lawson@education.gov.uk>
2025-02-24 18:14:49 +0000

    SWIP-207 - changed terraform providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,3 @@
-#Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.10.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
-
 # Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_name_prefix}-rg-webapp"
