36eef47aa733aff09b69332a04f73c6ed59ba65a
boblaw-dfe <armand.lawson@education.gov.uk>
2025-03-03 10:16:16 +0000

    SWIP-332 - Remove public net access setting as only on premium sku

--- terraform/modules/azure-container-registry/acr.tf
+++ terraform/modules/azure-container-registry/acr.tf
@@ -1,11 +1,11 @@
 resource "azurerm_container_registry" "acr" {
-  name                          = "${var.resource_name_prefix}acr"
-  resource_group_name           = var.resource_group
-  location                      = var.location
-  sku                           = var.acr_sku
-  admin_enabled                 = var.admin_enabled
-  public_network_access_enabled = var.public_network_access_enabled
-  tags                          = var.tags
+  name                = "${var.resource_name_prefix}acr"
+  resource_group_name = var.resource_group
+  location            = var.location
+  sku                 = var.acr_sku
+  admin_enabled       = var.admin_enabled
+  #public_network_access_enabled = var.public_network_access_enabled
+  tags = var.tags
 
   #checkov:skip=CKV_AZURE_164:Content Trusted images not available on Basic SKU
   #checkov:skip=CKV_AZURE_237:Dedicate data endpoint not available on Basic SKU
