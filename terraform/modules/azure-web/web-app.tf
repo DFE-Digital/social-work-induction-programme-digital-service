077324d08750d59281445bfce9bbfc84ea38b031
boblaw-dfe <armand.lawson@education.gov.uk>
2025-03-26 12:27:07 +0000

    SWIP-364 - Removed source control section

--- terraform/modules/azure-web/web-app.tf
+++ terraform/modules/azure-web/web-app.tf
@@ -137,11 +137,6 @@ resource "azurerm_linux_web_app" "webapp" {
   #checkov:skip=CKV_AZURE_213:Ensure that App Service configures health check
 }
 
-resource "azurerm_app_service_source_control" "appsource" {
-  app_id                 = azurerm_linux_web_app.webapp.id
-  use_manual_integration = false
-}
-
 resource "azurerm_monitor_diagnostic_setting" "webapp_logs_monitor" {
 
   name                       = "${var.resource_name_prefix}-webapp-mon"
