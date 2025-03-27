3ae2383c930f4cf20fb94bae7e2faf1355ad4f20
boblaw-dfe <armand.lawson@education.gov.uk>
2025-03-27 16:35:15 +0000

    SWIP-364 - Change KV access back to access policy

--- terraform/modules/azure-network/key-vault.tf
+++ terraform/modules/azure-network/key-vault.tf
@@ -10,6 +10,30 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days  = 7
   purge_protection_enabled    = true
   sku_name                    = "standard"
+
+  lifecycle {
+    ignore_changes = [
+      tags["Environment"],
+      tags["Product"],
+      tags["Service Offering"]
+    ]
+    create_before_destroy = true
+  }
+
+  #checkov:skip=CKV_AZURE_109:Access Policies configured
+  #checkov:skip=CKV_AZURE_189:Access Policies configured
+  #checkov:skip=CKV2_AZURE_32:VNET configuration adequate
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.resource_name_prefix}-kv"
+  resource_group_name         = var.resource_group
+  location                    = var.location
+  tenant_id                   = data.azurerm_client_config.az_config.tenant_id
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+  sku_name                    = "standard"
   access_policy               = []
   enable_rbac_authorization   = true
 
@@ -19,10 +43,8 @@ resource "azurerm_key_vault" "kv" {
       tags["Product"],
       tags["Service Offering"]
     ]
+    create_before_destroy = true
   }
 
-  #checkov:skip=CKV_AZURE_109:Access Policies configured
-  #checkov:skip=CKV_AZURE_189:Access Policies configured
   #checkov:skip=CKV2_AZURE_32:VNET configuration adequate
 }
-
