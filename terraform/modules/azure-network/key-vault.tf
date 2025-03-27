84db1845d2ae79f60db866ee32545aececae6d73
boblaw-dfe <armand.lawson@education.gov.uk>
2025-03-27 16:38:30 +0000

    SWIP-364 - Change KV access back to access policy

--- terraform/modules/azure-network/key-vault.tf
+++ terraform/modules/azure-network/key-vault.tf
@@ -17,7 +17,6 @@ resource "azurerm_key_vault" "kv" {
       tags["Product"],
       tags["Service Offering"]
     ]
-    create_before_destroy = true
   }
 
   #checkov:skip=CKV_AZURE_109:Access Policies configured
@@ -25,26 +24,41 @@ resource "azurerm_key_vault" "kv" {
   #checkov:skip=CKV2_AZURE_32:VNET configuration adequate
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                        = "${var.resource_name_prefix}-kv"
-  resource_group_name         = var.resource_group
-  location                    = var.location
-  tenant_id                   = data.azurerm_client_config.az_config.tenant_id
-  enabled_for_disk_encryption = true
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  sku_name                    = "standard"
-  access_policy               = []
-  enable_rbac_authorization   = true
+# Access Policy for GitHub Actions
+resource "azurerm_key_vault_access_policy" "kv_gh_ap" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.az_config.tenant_id
+  object_id    = data.azurerm_client_config.az_config.object_id
+
+  key_permissions = [
+    "Create",
+    "Delete",
+    "Get",
+    "UnwrapKey",
+    "WrapKey",
+    "Recover",
+    "Purge",
+    "Update",
+    "GetRotationPolicy",
+    "SetRotationPolicy"
+  ]
+
+  secret_permissions = ["List", "Get", "Set", "Recover"]
+
+  certificate_permissions = [
+    "Create",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "SetIssuers",
+    "Update"
+  ]
 
   lifecycle {
-    ignore_changes = [
-      tags["Environment"],
-      tags["Product"],
-      tags["Service Offering"]
-    ]
-    create_before_destroy = true
+    ignore_changes = [object_id]
   }
-
-  #checkov:skip=CKV2_AZURE_32:VNET configuration adequate
 }
