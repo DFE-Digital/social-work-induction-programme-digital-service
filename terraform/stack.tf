82f0958c539d2a1bd612869c067ac90b6a46dadc
paul <armstrong.pv@gmail.com>
2025-04-22 11:52:39 +0100

    Fix Terraform linting

--- terraform/stack.tf
+++ terraform/stack.tf
@@ -6,7 +6,7 @@ module "stack" {
   primary_resource_group                     = var.primary_resource_group
   resource_name_prefix                       = var.resource_name_prefix
   kv_purge_protection_enabled                = var.kv_purge_protection_enabled
-  asp_sku_moodle                                    = var.asp_sku_moodle
+  asp_sku_moodle                             = var.asp_sku_moodle
   admin_enabled                              = var.admin_enabled
   webapp_storage_account_name                = var.webapp_storage_account_name
   assign_delivery_team_key_vault_permissions = var.assign_delivery_team_key_vault_permissions
