aca1a9bd5e8fa4daff0c40edf7f65e04a182135f
pva-dfe <paul.armstrong@education.gov.uk>
2025-04-25 10:33:05 +0100

    Support extra d02 environment in the dev subscription and for the Dev github actions environment (#43)
    Details:
    
    Container registry is shared
    Azure dev subscription is reused
    Azure environment prefix is d02... and has separate resource / Terraform state resource groups
    Github environment Dev is reused (could be changed in future but requires Azure federated credentials to support a new Dev02 Github environment)
    When deploying, the user selects the environment instance in the workflow (d01 / d02)

--- terraform/azure-container-registry.tf
+++ terraform/azure-container-registry.tf
@@ -1,4 +1,14 @@
+// TODO: This will work while we create environments within the same dev subscription, but
+// not when we start using separate test and production subscriptions.
+
+data "azurerm_container_registry" "acr_data" {
+  count               = var.create_and_own_container_registry ? 0 : 1
+  name                = var.acr_name
+  resource_group_name = var.acr_resource_group
+}
+
 resource "azurerm_container_registry" "acr" {
+  count               = var.create_and_own_container_registry ? 1 : 0
   name                = var.acr_name
   resource_group_name = module.stack.resource_group_name
   location            = var.azure_region
@@ -18,4 +28,8 @@ resource "azurerm_container_registry" "acr" {
   #checkov:skip=CKV_AZURE_166:Image quarantine not available on Basic SKU
   #checkov:skip=CKV_AZURE_167:Retention policy not available on Basic SKU
   #checkov:skip=CKV_AZURE_139:Disable public network access not available on Basic SKU
+}
+
+locals {
+  acr_id = var.create_and_own_container_registry ? azurerm_container_registry.acr[0].id : data.azurerm_container_registry.acr_data[0].id
 }
