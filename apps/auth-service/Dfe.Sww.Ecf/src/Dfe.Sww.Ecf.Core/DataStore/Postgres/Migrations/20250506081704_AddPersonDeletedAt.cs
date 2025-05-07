896fe0b5f35a99eb95e5e46fd31a0c9b029fd657
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2025-05-07 13:45:35 +0100

    add functionality to soft delete user (#49)

---
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Migrations/20250506081704_AddPersonDeletedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Sww.Ecf.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonDeletedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deleted_at",
+                table: "persons",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deleted_at",
+                table: "persons");
+        }
+    }
+}
