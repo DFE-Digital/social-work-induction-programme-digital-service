2a74ff728de0c5968643e04f89bac31e46c35170
paul <armstrong.pv@gmail.com>
2025-05-12 14:46:25 +0000

    First commit

--- terraform/modules/web-app/outputs.tf
+++ terraform/modules/web-app/outputs.tf
@@ -12,3 +12,8 @@ output "web_app_url" {
   description = "Default HTTPS URL of the Linux web app"
   value       = "https://${azurerm_linux_web_app.webapp.default_hostname}"
 }
+
+output "front_door_app_url" {
+  description = "Front Door HTTPS URL of the Linux web app"
+  value       = "https://${azurerm_cdn_frontdoor_endpoint.front_door_endpoint_web.host_name}"
+}
