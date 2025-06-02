6664b881afacda9fa31937bed297b9ad6edf967d
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-06-02 17:27:47 +0100

    Add ecsw programme start/end dates page

---
+++ apps/user-management/apps/frontend/Validation/SocialWorkerProgrammeDatesValidator.cs
@@ -0,0 +1,25 @@
+using Dfe.Sww.Ecf.Frontend.Pages.ManageAccounts;
+using FluentValidation;
+using NodaTime;
+
+namespace Dfe.Sww.Ecf.Frontend.Validation;
+
+public class SocialWorkerProgrammeDatesValidator : AbstractValidator<SocialWorkerProgrammeDates>
+{
+    public SocialWorkerProgrammeDatesValidator()
+    {
+        When(
+            x => x.ProgrammeEndDate.HasValue,
+            () => RuleFor(x => x.ProgrammeEndDate)
+                .Must(BeInTheFuture)
+                .WithMessage("Expected programme end date must be in the future"));
+    }
+
+    private bool BeInTheFuture(YearMonth? date)
+    {
+        var now = DateTime.UtcNow;
+        var currentYearMonth = new YearMonth(now.Year, now.Month);
+
+        return date > currentYearMonth;
+    }
+}
