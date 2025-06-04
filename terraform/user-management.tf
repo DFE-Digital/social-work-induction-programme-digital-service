4da28240d0f85cfd91fc7a8289aa945aebecf6a0
boblaw-dfe <armand.lawson@education.gov.uk>
2025-06-04 15:44:55 +0100

    Add missing parameters to other web app modules

--- terraform/user-management.tf
+++ terraform/user-management.tf
@@ -4,24 +4,25 @@ locals {
 }
 
 module "user_management" {
-  source                    = "./modules/web-app"
-  tenant_id                 = data.azurerm_client_config.az_config.tenant_id
-  environment               = var.environment
-  location                  = var.azure_region
-  resource_group            = module.stack.resource_group_name
-  resource_name_prefix      = var.resource_name_prefix
-  web_app_name              = "${var.resource_name_prefix}-wa-user-management"
-  web_app_short_name        = "wa-user-management"
-  docker_image_name         = "dfe-digital/nothing:latest"
-  front_door_profile_web_id = module.stack.front_door_profile_web_id
-  subnet_webapps_id         = module.stack.subnet_services_id
-  acr_id                    = local.acr_id
-  acr_name                  = var.acr_name
-  key_vault_id              = module.stack.kv_id
-  service_plan_id           = module.stack.services_service_plan_id
-  health_check_path         = "/version.txt"
-  support_action_group_id   = module.stack.support_action_group_id
-  tags                      = local.common_tags
+  source                        = "./modules/web-app"
+  tenant_id                     = data.azurerm_client_config.az_config.tenant_id
+  environment                   = var.environment
+  location                      = var.azure_region
+  resource_group                = module.stack.resource_group_name
+  resource_name_prefix          = var.resource_name_prefix
+  web_app_name                  = "${var.resource_name_prefix}-wa-user-management"
+  web_app_short_name            = "wa-user-management"
+  docker_image_name             = "dfe-digital/nothing:latest"
+  front_door_profile_web_id     = module.stack.front_door_profile_web_id
+  subnet_webapps_id             = module.stack.subnet_services_id
+  acr_id                        = local.acr_id
+  acr_name                      = var.acr_name
+  key_vault_id                  = module.stack.kv_id
+  service_plan_id               = module.stack.services_service_plan_id
+  health_check_path             = "/version.txt"
+  support_action_group_id       = module.stack.support_action_group_id
+  appinsights_connection_string = module.stack.appinsights_connection_string
+  tags                          = local.common_tags
 
   # The settings name syntax below (e.g. OIDC__AUTHORITYURL) is how .NET imports environment 
   # variables to override the properties in its multi-level appsettings.json file
