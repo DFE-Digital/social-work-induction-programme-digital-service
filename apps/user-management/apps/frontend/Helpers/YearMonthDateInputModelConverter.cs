03e08e1b3008af41f16ec7d0ec9fddd99f7608fb
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2025-06-04 15:14:01 +0100

    Add ecsw programme start/end dates page (#86)

---
+++ apps/user-management/apps/frontend/Helpers/YearMonthDateInputModelConverter.cs
@@ -0,0 +1,21 @@
+﻿using GovUk.Frontend.AspNetCore;
+using GovUk.Frontend.AspNetCore.ModelBinding;
+using NodaTime;
+
+namespace Dfe.Sww.Ecf.Frontend.Helpers;
+
+public class YearMonthDateInputModelConverter : DateInputModelConverter
+{
+    public override DateInputItemTypes? DefaultItemTypes => DateInputItemTypes.MonthAndYear;
+
+    protected override object ConvertToModelCore(DateInputConvertToModelContext context)
+    {
+        return new YearMonth(context.ItemValues.Year!.Value, context.ItemValues.Month!.Value);
+    }
+
+    protected override DateInputItemValues? ConvertFromModelCore(DateInputConvertFromModelContext context)
+    {
+        var yearMonth = (YearMonth)context.Model;
+        return new DateInputItemValues(null, yearMonth.Month, yearMonth.Year);
+    }
+}
