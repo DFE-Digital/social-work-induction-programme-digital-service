ca8c84b898b635d8b3e548bc24f67ed39b8c0a26
Igi <brad@igi.codes>
2025-06-17 14:18:54 +0100

    SWIP-627 - Add unit tests for new ViewAccountDetails props

--- apps/user-management/apps/frontend.Test/UnitTests/Pages/ManageAccounts/ViewAccountDetailsPageTests.cs
+++ apps/user-management/apps/frontend.Test/UnitTests/Pages/ManageAccounts/ViewAccountDetailsPageTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+using Dfe.Sww.Ecf.Frontend.Models;
 using Dfe.Sww.Ecf.Frontend.Pages.ManageAccounts;
 using Dfe.Sww.Ecf.Frontend.Test.UnitTests.Helpers;
 using FluentAssertions;
@@ -9,8 +11,6 @@ namespace Dfe.Sww.Ecf.Frontend.Test.UnitTests.Pages.ManageAccounts;
 
 public class ViewAccountDetailsPageTests : ManageAccountsPageTestBase<ViewAccountDetails>
 {
-    private ViewAccountDetails Sut { get; }
-
     public ViewAccountDetailsPageTests()
     {
         Sut = new ViewAccountDetails(MockAccountService.Object, new FakeLinkGenerator())
@@ -18,6 +18,8 @@ public class ViewAccountDetailsPageTests : ManageAccountsPageTestBase<ViewAccoun
             TempData = TempData
         };
     }
+
+    private ViewAccountDetails Sut { get; }
 
     [Fact]
     public async Task Get_WhenCalledWithId_LoadsTheView()
@@ -33,6 +35,50 @@ public class ViewAccountDetailsPageTests : ManageAccountsPageTestBase<ViewAccoun
         // Assert
         result.Should().BeOfType<PageResult>();
         Sut.Account.Should().BeEquivalentTo(account);
+
+        MockAccountService.Verify(x => x.GetByIdAsync(account.Id), Times.Once);
+        MockAccountService.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task Get_WhenCalledWithSocialWorker_SetsIsSocialWorkerToTrue()
+    {
+        // Arrange
+        var account = AccountBuilder.WithTypes([AccountType.EarlyCareerSocialWorker]).Build();
+
+        MockAccountService.Setup(x => x.GetByIdAsync(account.Id)).ReturnsAsync(account);
+
+        // Act
+        var result = await Sut.OnGetAsync(account.Id);
+
+        // Assert
+        result.Should().BeOfType<PageResult>();
+        Sut.Account.Should().BeEquivalentTo(account);
+        Sut.IsSocialWorker.Should().BeTrue();
+        Sut.IsAssessor.Should().BeFalse();
+
+        MockAccountService.Verify(x => x.GetByIdAsync(account.Id), Times.Once);
+        MockAccountService.VerifyNoOtherCalls();
+    }
+
+    [Theory]
+    [InlineData(new[] { AccountType.Assessor })]
+    [InlineData(new[] { AccountType.Assessor, AccountType.Coordinator })]
+    public async Task Get_WhenCalledWithAssessor_SetsIsAssessorToTrue(AccountType[] accountTypes)
+    {
+        // Arrange
+        var account = AccountBuilder.WithTypes(accountTypes.ToImmutableList()).Build();
+
+        MockAccountService.Setup(x => x.GetByIdAsync(account.Id)).ReturnsAsync(account);
+
+        // Act
+        var result = await Sut.OnGetAsync(account.Id);
+
+        // Assert
+        result.Should().BeOfType<PageResult>();
+        Sut.Account.Should().BeEquivalentTo(account);
+        Sut.IsSocialWorker.Should().BeFalse();
+        Sut.IsAssessor.Should().BeTrue();
 
         MockAccountService.Verify(x => x.GetByIdAsync(account.Id), Times.Once);
         MockAccountService.VerifyNoOtherCalls();
