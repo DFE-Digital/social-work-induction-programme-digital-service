2579a7edea25267fa65fd44c3cbaa35c79f93f4c
boblaw-dfe <armand.lawson@education.gov.uk>
2025-06-18 16:07:58 +0100

    SWIP-693 - Initial terraform for notification service

---
+++ terraform/modules/function-app/variables.tf
@@ -0,0 +1,45 @@
+variable "environment" {
+  description = "Environment to deploy resources"
+  type        = string
+}
+
+variable "location" {
+  description = "Name of the Azure region to deploy resources"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "Name of the Azure Resource Group to deploy resources"
+  type        = string
+}
+
+variable "resource_name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+}
+
+variable "service_plan_id" {
+  description = "ID of the app service plan"
+  type        = string
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry name"
+  type        = string
+}
+
+variable "function_app_name" {
+  description = "Name of the function app"
+  type        = string
+  default     = ""
+}
+
+variable "docker_image_name" {
+  description = "The Docker image name to use for the web app"
+  type        = string
+}
