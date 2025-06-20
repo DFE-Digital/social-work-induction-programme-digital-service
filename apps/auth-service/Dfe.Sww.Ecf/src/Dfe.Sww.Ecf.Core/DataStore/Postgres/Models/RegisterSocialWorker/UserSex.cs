5f85909444f7256cb18b260a30138ec107012b68
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-06-20 16:52:44 +0100

    add reg flow questions to update

---
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Models/RegisterSocialWorker/UserSex.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dfe.Sww.Ecf.Core.DataStore.Postgres.Models.RegisterSocialWorker;
+
+public enum UserSex
+{
+    [Display(Name = "Prefer not to say")] PreferNotToSay,
+
+    [Display(Name = "Female")] Female,
+
+    [Display(Name = "Male")] Male
+}
