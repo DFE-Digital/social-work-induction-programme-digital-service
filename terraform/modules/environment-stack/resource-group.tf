9c323d9358e4f1b20acd15467a671d6f39292d1c
paul <armstrong.pv@gmail.com>
2025-06-20 10:20:33 +0000

    Fix

--- terraform/modules/environment-stack/resource-group.tf
+++ terraform/modules/environment-stack/resource-group.tf
@@ -1,9 +1,10 @@
 data "azurerm_client_config" "az_config" {}
 
 resource "azurerm_resource_group" "rg_primary" {
-  name     = var.primary_resource_group
-  location = var.location
-  tags     = var.tags
+  name       = var.primary_resource_group
+  location   = var.location
+  managed_by = data.azurerm_client_config.az_config.object_id
+  tags       = var.tags
 
   lifecycle {
     ignore_changes = [tags]
@@ -14,9 +15,9 @@ resource "azurerm_resource_group" "rg_primary" {
 # Terraform is not able to perform more advanced role assignments such as assigning the
 # "Storage File Data SMB Share Contributor" role to a web app managed identity.
 
-resource "azurerm_role_assignment" "tf_sp_owner_on_rg" {
-  scope                = azurerm_resource_group.rg_primary.id
-  role_definition_name = "Owner"
-  principal_type       = "ServicePrincipal"
-  principal_id         = data.azurerm_client_config.az_config.object_id
-}
+# resource "azurerm_role_assignment" "tf_sp_owner_on_rg" {
+#   scope                = azurerm_resource_group.rg_primary.id
+#   role_definition_name = "Owner"
+#   principal_type       = "ServicePrincipal"
+#   principal_id         = data.azurerm_client_config.az_config.object_id
+# }
