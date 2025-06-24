1dc32cb2ff3351be5fb16ca989a9dee63897dfbf
Paul Armstrong <paul.armstrong@education.gov.uk>
2025-06-24 17:06:53 +0100

    Fixes

--- terraform/modules/environment-stack/resource-group.tf
+++ terraform/modules/environment-stack/resource-group.tf
@@ -3,7 +3,6 @@ data "azurerm_client_config" "az_config" {}
 resource "azurerm_resource_group" "rg_primary" {
   name       = var.primary_resource_group
   location   = var.location
-  managed_by = data.azurerm_client_config.az_config.object_id
   tags       = var.tags
 
   lifecycle {
