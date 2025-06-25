adbbc9f78e8715266a746e95f364b00a274d59dd
boblaw-dfe <armand.lawson@education.gov.uk>
2025-06-25 16:22:01 +0100

    Set principal type to ServicePrincipal

--- terraform/modules/function-app/function-app.tf
+++ terraform/modules/function-app/function-app.tf
@@ -35,5 +35,6 @@ resource "azurerm_linux_function_app" "function_app" {
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = var.acr_id
   role_definition_name = "AcrPull"
+  principal_type       = "ServicePrincipal"
   principal_id         = azurerm_linux_function_app.function_app.identity[0].principal_id
 }
