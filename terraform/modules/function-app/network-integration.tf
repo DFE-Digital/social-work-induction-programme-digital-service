effdabbd358daa198dc36c41db7a55dfe41404c6
boblaw-dfe <armand.lawson@education.gov.uk>
2025-07-03 09:58:24 +0100

    Change function app vnet integration name

--- terraform/modules/function-app/network-integration.tf
+++ terraform/modules/function-app/network-integration.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_virtual_network_swift_connection" "asvnsc_funcapp" {
+resource "azurerm_app_service_virtual_network_swift_connection" "asvnsc_functionapp" {
   app_service_id = azurerm_linux_function_app.function_app.id
   subnet_id      = var.subnet_functionapp_id
 }
