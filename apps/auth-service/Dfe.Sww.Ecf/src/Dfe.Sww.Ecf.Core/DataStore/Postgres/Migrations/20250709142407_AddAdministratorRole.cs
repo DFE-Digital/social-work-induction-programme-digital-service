4f1bfc6badf1d5c034c6c38c8f65b9b1e4b57403
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-07-10 11:15:28 +0100

    Add administrator role (#160)

---
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Migrations/20250709142407_AddAdministratorRole.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Sww.Ecf.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdministratorRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_roles_role_name",
+                table: "roles");
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "role_id", "role_name" },
+                values: new object[] { 1000, "Administrator" });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_roles_role_name",
+                table: "roles",
+                sql: "role_name in ('EarlyCareerSocialWorker', 'Assessor', 'Coordinator', 'Administrator')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_roles_role_name",
+                table: "roles");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "role_id",
+                keyValue: 1000);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_roles_role_name",
+                table: "roles",
+                sql: "role_name in ('EarlyCareerSocialWorker', 'Assessor', 'Coordinator')");
+        }
+    }
+}
