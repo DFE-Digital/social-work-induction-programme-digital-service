2a7460a195e0306fe202ed0bfb910cd991812c1e
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-07-17 16:20:59 +0100

    add ToAccount method

--- apps/user-management/apps/frontend/Models/AccountDetails.cs
+++ apps/user-management/apps/frontend/Models/AccountDetails.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
 using Dfe.Sww.Ecf.Frontend.Models.RegisterSocialWorker;
 
@@ -125,6 +126,25 @@ public class AccountDetails
             Types = account.Types,
             PhoneNumber = account.PhoneNumber,
             PhoneNumberRequired = account.PhoneNumberRequired
+        };
+    }
+
+    public static Account ToAccount(AccountDetails accountDetails)
+    {
+        return new Account
+        {
+            FirstName = accountDetails.FirstName,
+            MiddleNames = accountDetails.MiddleNames,
+            LastName = accountDetails.LastName,
+            Email = accountDetails.Email,
+            PhoneNumber = accountDetails.PhoneNumber,
+            Status = AccountStatus.Active,
+            Types = accountDetails.Types?.ToImmutableList(),
+            SocialWorkEnglandNumber = accountDetails.SocialWorkEnglandNumber,
+            ProgrammeStartDate = accountDetails.ProgrammeStartDate,
+            ProgrammeEndDate = accountDetails.ProgrammeEndDate,
+            ExternalUserId = accountDetails.ExternalUserId,
+            IsFunded = accountDetails.IsFunded
         };
     }
 }
