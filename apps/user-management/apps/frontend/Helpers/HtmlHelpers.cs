3e8c7817296b9f26457d73f182edf103390b6176
Brad Park (Igi) <brad@igi.codes>
2025-07-22 16:24:36 +0100

    SWIP-867 - Update user management GovUK branding (#183)
    Co-authored-by: Sam Nicholson <sam.nicholson@hippodigital.co.uk>

--- apps/user-management/apps/frontend/Helpers/HtmlHelpers.cs
+++ apps/user-management/apps/frontend/Helpers/HtmlHelpers.cs
@@ -7,7 +7,7 @@ public static class HtmlHelpers
     public static string IsActive(
         this IHtmlHelper html,
         string page,
-        string cssClass = "service-header__nav-list-item--active"
+        string cssClass = "govuk-service-navigation__item--active"
     )
     {
         var currentPage = html.ViewContext.RouteData.Values["page"]?.ToString();
