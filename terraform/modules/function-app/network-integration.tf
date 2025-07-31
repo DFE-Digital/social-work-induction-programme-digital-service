51cbba8463ebbd9ecf070c2526e27794db7241ed
Igi <brad@igi.codes>
2025-07-31 12:29:17 +0100

    SWIP-960 - Fix resource naming

--- terraform/modules/function-app/network-integration.tf
+++ terraform/modules/function-app/network-integration.tf
@@ -19,14 +19,14 @@ resource "azurerm_private_dns_zone" "pvt_dns_zone" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
-  name                  = "${data.azurerm_virtual_network.main.name}-dns-link"
+  name                  = "${data.azurerm_virtual_network.vnet_stack.name}-dns-link"
   resource_group_name   = var.resource_group
   private_dns_zone_name = azurerm_private_dns_zone.pvt_dns_zone.name
   virtual_network_id    = var.virtual_network_id
 }
 
 resource "azurerm_private_endpoint" "function_app_endpoint" {
-  name                = "${data.azurerm_function_app.main.name}-endpoint"
+  name                = "${data.azurerm_function_app.function_app.name}-endpoint"
   location            = var.location
   resource_group_name = var.resource_group
   subnet_id           = var.subnet_functionapp_id
@@ -39,7 +39,7 @@ resource "azurerm_private_endpoint" "function_app_endpoint" {
 
   # Connect the endpoint to the Function App.
   private_service_connection {
-    name                           = "${data.azurerm_function_app.main.name}-connection"
+    name                           = "${data.azurerm_function_app.function_app.name}-connection"
     is_manual_connection           = false
     private_connection_resource_id = azurerm_linux_function_app.function_app.id
     subresource_names              = ["sites"]
