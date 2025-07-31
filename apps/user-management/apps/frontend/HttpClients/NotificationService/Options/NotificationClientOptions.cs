291a04a2e098bce24d0a47515c384070807a6373
Igi <brad@igi.codes>
2025-07-31 15:38:54 +0100

    SWIP-960 - Add function key header to notification requests

--- apps/user-management/apps/frontend/HttpClients/NotificationService/Options/NotificationClientOptions.cs
+++ apps/user-management/apps/frontend/HttpClients/NotificationService/Options/NotificationClientOptions.cs
@@ -5,4 +5,5 @@ namespace Dfe.Sww.Ecf.Frontend.HttpClients.NotificationService.Options;
 public class NotificationClientOptions : HttpClientOptions
 {
     public required NotificationServiceRoutes Routes { get; init; }
+    public string? FunctionKey { get; init; } = null;
 }
