33b72b9d3a7ab1c8cb481cc2bf3beed5f2d4430e
Igi <brad@igi.codes>
2025-07-31 14:10:27 +0100

    SWIP-960 - Revent baseurl back to main endpoint

--- terraform/user-management.tf
+++ terraform/user-management.tf
@@ -36,7 +36,7 @@ module "user_management" {
     "AUTHCLIENTOPTIONS__BASEURL"              = module.auth_service.front_door_app_url
     "MOODLECLIENTOPTIONS__BASEURL"            = "${module.web_app_moodle["primary"].front_door_app_url}/webservice/rest/server.php"
     "MOODLECLIENTOPTIONS__APITOKEN"           = "@Microsoft.KeyVault(SecretUri=${module.stack.kv_vault_uri}secrets/${azurerm_key_vault_secret.web_service_token.name})"
-    "NOTIFICATIONCLIENTOPTIONS__BASEURL"      = module.notification-service.function_app_private_url
+    "NOTIFICATIONCLIENTOPTIONS__BASEURL"      = module.notification-service.function_app_url
     "NOTIFICATIONCLIENTOPTIONS__FUNCTIONKEY"  = "@Microsoft.KeyVault(SecretUri=${module.stack.kv_vault_uri}secrets/${azurerm_key_vault_secret.function_key.name})"
     "BASIC_AUTH_USER"                         = var.basic_auth_user
     "BASIC_AUTH_PASSWORD"                     = "@Microsoft.KeyVault(SecretUri=${module.stack.kv_vault_uri}secrets/Sites-BasicAuthPassword)"
