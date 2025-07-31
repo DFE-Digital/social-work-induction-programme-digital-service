ef235b87af45fb707fd2ddd8731ff6a20db50d5b
Igi <brad@igi.codes>
2025-07-31 20:45:37 +0100

    SWIP-960 - Ensure function keys aren't fetched until function deployed

--- terraform/modules/function-app/function-app.tf
+++ terraform/modules/function-app/function-app.tf
@@ -95,4 +95,6 @@ resource "azurerm_key_vault_access_policy" "kv_policy" {
 data "azurerm_function_app_host_keys" "function_keys" {
   name                = azurerm_linux_function_app.function_app.name
   resource_group_name = var.resource_group
+
+  depends_on = [azurerm_linux_function_app.function_app]
 }
