97c73e8fd2296dc212b43eccdc3ac74a49719597
Bob Lawson <armand.lawson@education.gov.uk>
2025-08-04 17:18:54 +0100

    SWIP-519 Environment sizing (#205)

--- terraform/modules/environment-stack/key-vault.tf
+++ terraform/modules/environment-stack/key-vault.tf
@@ -53,25 +53,25 @@ locals {
     # For convenience, we give the delivery team read / write access to secrets in non-prod envs
     # Note that they can do this anyway manually, but Terraform will reset to known state
     delivery_team_user_group = {
-      object_id = "42d30dac-805f-42e7-82fc-6b70f5c4649b" # s205-earlycareerframeworkforsocialwork-Delivery Team
+      object_id          = "42d30dac-805f-42e7-82fc-6b70f5c4649b" # s205-earlycareerframeworkforsocialwork-Delivery Team
       assign_permissions = var.assign_delivery_team_key_vault_permissions
     }
     github_actions = {
-      object_id = data.azurerm_client_config.az_config.object_id
+      object_id          = data.azurerm_client_config.az_config.object_id
       assign_permissions = true
     }
   }
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                         = "${var.resource_name_prefix}-kv-primary"
-  resource_group_name          = azurerm_resource_group.rg_primary.name
-  location                     = var.location
-  tenant_id                    = data.azurerm_client_config.az_config.tenant_id
-  enabled_for_disk_encryption  = true
-  soft_delete_retention_days   = 7
-  purge_protection_enabled     = var.kv_purge_protection_enabled
-  sku_name                     = "standard"
+  name                        = "${var.resource_name_prefix}-kv-primary"
+  resource_group_name         = azurerm_resource_group.rg_primary.name
+  location                    = var.location
+  tenant_id                   = data.azurerm_client_config.az_config.tenant_id
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = var.kv_purge_protection_enabled
+  sku_name                    = var.key_vault_sku
 
   lifecycle {
     ignore_changes = [tags]
@@ -88,8 +88,8 @@ resource "azurerm_key_vault_access_policy" "kv_gh_ap" {
   tenant_id    = data.azurerm_client_config.az_config.tenant_id
   object_id    = each.value.object_id
 
-  key_permissions = local.principal_key_permissions
-  secret_permissions = local.principal_secret_permissions
+  key_permissions         = local.principal_key_permissions
+  secret_permissions      = local.principal_secret_permissions
   certificate_permissions = local.principal_certificate_permissions
 
   lifecycle {
