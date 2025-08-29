986a30f0627049bed4e1babaa8defe7d46c6cbd2
boblaw-dfe <armand.lawson@education.gov.uk>
2025-08-29 18:01:07 +0100

    Added Front Door route to function app

--- terraform/db-jobs-app.tf
+++ terraform/db-jobs-app.tf
@@ -27,3 +27,55 @@ module "db-jobs-app" {
     "DB_PASSWORD"                          = "@Microsoft.KeyVault(SecretUri=${module.stack.full_postgres_secret_password_uri})"
   }, var.db_jobs_app_settings)
 }
+
+# Front Door route for this function app only
+resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
+  name                     = "fd-ops-trigger-${random_string.suffix.result}"
+  cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
+  tags                     = local.common_tags
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
+  name                     = "${var.resource_name_prefix}-fd-origin-group-web-fa-db-operations"
+  cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
+
+  health_probe {
+    path                = "/api/health"
+    protocol            = "Https"
+    interval_in_seconds = 60
+  }
+
+  load_balancing {
+    sample_size                 = 4
+    successful_samples_required = 2
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "origin" {
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin_group.id
+  enabled                        = true
+  certificate_name_check_enabled = false
+  host_name                      = module.function_app.function_app_default_hostname
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = module.function_app.function_app_default_hostname
+  priority                       = 1
+  weight                         = 1
+  name                           = "${var.resource_name_prefix}-fd-origin-web-fa-db-operations"
+}
+
+resource "azurerm_cdn_frontdoor_route" "route" {
+  name                          = "${var.resource_name_prefix}-fd-route-web-fa-db-operations"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.front_door_endpoint_web.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.front_door_origin_group_web.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.front_door_origin_web.id]
+
+  forwarding_protocol    = "MatchRequest"
+  https_redirect_enabled = true
+  patterns_to_match      = ["/*"]
+  supported_protocols    = ["Http", "Https"]
+}
