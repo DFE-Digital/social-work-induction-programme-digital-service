a5ade4b107515c84baf94d14e32b78fd8832c3eb
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-09 16:27:31 +0100

    Custom nav for learners in GOVUK SWPDP theme (#231)
    * Custom nav for learners in GOVUK SWPDP theme
    
    * Remove moodle copyright
    
    * Tweak parents order
    
    This should influence order of styles being loaded (govuk last to take precedence, although for style extensions this doesn't seem to apply)

---
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/config.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'govuk_swpdp';
+
+$THEME->parents = ['boost', 'govuk'];
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
