f7c4a86da736e2bea227be349547e39b4a803ad6
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-09 15:55:27 +0100

    Remove moodle copyright

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/lang/en/theme_govuk_swpdp.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/lang/en/theme_govuk_swpdp.php
@@ -1,27 +1,4 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Theme functions.
- *
- * @package    theme_govuk_swpdp
- * @copyright  Copyright (c) Crown Copyright (Department for Education)
- * @license    https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/ Open Government Licence v3.0
- */
-
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
