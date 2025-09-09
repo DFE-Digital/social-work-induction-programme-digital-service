1427f33d6e7c2a30b39a40b0f6febaafaa12a958
Igi <brad@igi.codes>
2025-09-09 18:56:02 +0100

    Re-enable origin resources

--- terraform/db-jobs-app.tf
+++ terraform/db-jobs-app.tf
@@ -28,63 +28,63 @@ module "db-jobs-app" {
   }, var.db_jobs_app_settings)
 }
 
-# # Front Door route for this function app only
-# resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
-#   name                     = "${var.resource_name_prefix}-fd-ops-trigger"
-#   cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
-#   tags                     = local.common_tags
+# Front Door route for this function app only
+resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
+  name                     = "${var.resource_name_prefix}-fd-ops-trigger"
+  cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
+  tags                     = local.common_tags
 
-#   lifecycle {
-#     ignore_changes = [tags]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
 
-# resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
-#   name                     = "${var.resource_name_prefix}-fd-origin-group-web-fa-db-operations"
-#   cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
+resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
+  name                     = "${var.resource_name_prefix}-fd-origin-group-web-fa-db-operations"
+  cdn_frontdoor_profile_id = module.stack.front_door_profile_web_id
 
-#   health_probe {
-#     path                = "/api/health"
-#     protocol            = "Https"
-#     interval_in_seconds = 60
-#   }
+  health_probe {
+    path                = "/api/health"
+    protocol            = "Https"
+    interval_in_seconds = 60
+  }
 
-#   load_balancing {
-#     sample_size                 = 4
-#     successful_samples_required = 2
-#   }
-# }
+  load_balancing {
+    sample_size                 = 4
+    successful_samples_required = 2
+  }
+}
 
-# resource "azurerm_cdn_frontdoor_origin" "origin" {
-#   cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin_group.id
-#   enabled                        = true
-#   certificate_name_check_enabled = false
-#   host_name                      = module.db-jobs-app.function_app_default_hostname
-#   http_port                      = 80
-#   https_port                     = 443
-#   origin_host_header             = module.db-jobs-app.function_app_default_hostname
-#   priority                       = 1
-#   weight                         = 1
-#   name                           = "${var.resource_name_prefix}-fd-origin-web-fa-db-operations"
+resource "azurerm_cdn_frontdoor_origin" "origin" {
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin_group.id
+  enabled                        = true
+  certificate_name_check_enabled = false
+  host_name                      = module.db-jobs-app.function_app_default_hostname
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = module.db-jobs-app.function_app_default_hostname
+  priority                       = 1
+  weight                         = 1
+  name                           = "${var.resource_name_prefix}-fd-origin-web-fa-db-operations"
 
-#   private_link {
-#     location               = var.azure_region
-#     private_link_target_id = module.db-jobs-app.function_app_id
-#     target_type            = "sites"
-#   }
-# }
+  private_link {
+    location               = var.azure_region
+    private_link_target_id = module.db-jobs-app.function_app_id
+    target_type            = "sites"
+  }
+}
 
-# resource "azurerm_cdn_frontdoor_route" "route" {
-#   name                          = "${var.resource_name_prefix}-fd-route-web-fa-db-operations"
-#   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group.id
-#   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.origin.id]
+resource "azurerm_cdn_frontdoor_route" "route" {
+  name                          = "${var.resource_name_prefix}-fd-route-web-fa-db-operations"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.origin.id]
 
-#   forwarding_protocol    = "MatchRequest"
-#   https_redirect_enabled = true
-#   patterns_to_match      = ["/*"]
-#   supported_protocols    = ["Http", "Https"]
-# }
+  forwarding_protocol    = "MatchRequest"
+  https_redirect_enabled = true
+  patterns_to_match      = ["/*"]
+  supported_protocols    = ["Http", "Https"]
+}
 
 # Retrieve the default function key from the deployed function app
 data "azurerm_function_app_host_keys" "db_operations_keys" {
