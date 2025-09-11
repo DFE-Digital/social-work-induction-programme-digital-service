10c6bc63e52cc9da082e1d69f7cb3b912fb522bc
Igi <brad@igi.codes>
2025-09-11 18:25:48 +0100

    Define magic-links module on stack

--- terraform/modules/environment-stack/front-door.tf
+++ terraform/modules/environment-stack/front-door.tf
@@ -8,3 +8,20 @@ resource "azurerm_cdn_frontdoor_profile" "front_door_profile_web" {
     ignore_changes = [tags]
   }
 }
+
+# Magic Links Module - Only created when enabled
+module "magic-links" {
+  count  = var.magic_links_enabled ? 1 : 0
+  source = "./magic-links"
+
+  resource_name_prefix = var.resource_name_prefix
+  magic_links_enabled  = var.magic_links_enabled
+  moodle_instances     = var.moodle_instances
+  frontdoor_sku        = var.frontdoor_sku
+  tags                 = var.tags
+
+  key_vault_id           = azurerm_key_vault.kv.id
+  frontdoor_profile_id   = azurerm_cdn_frontdoor_profile.front_door_profile_web.id
+  frontdoor_profile_name = azurerm_cdn_frontdoor_profile.front_door_profile_web.name
+  resource_group_name    = azurerm_resource_group.rg_primary.name
+}
