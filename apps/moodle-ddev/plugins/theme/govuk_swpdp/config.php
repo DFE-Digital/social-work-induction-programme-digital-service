d462b3e2fecfda9b79098d38f48daa4748e77ade
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-11 10:39:59 +0100

    SWIP-1054 Tweaks to swpdp moodle theme and removal of theme specified in config (#235)
    * Change to child theme config to preserve templates
    
    For govuk templates which overwrite boost template, govuk must be listed as the first parent
    
    * Remove enforcing a theme in config

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/config.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/config.php
@@ -3,6 +3,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $THEME->name = 'govuk_swpdp';
 
-$THEME->parents = ['boost', 'govuk'];
+/**
+ * To inherit both govuk and boost styles, both themes need listing as parents.
+ * The order of the parents matters. For this theme to inherit
+ * govuk templates that overwrite boost templates,
+ * govuk must be listed first
+ */
+$THEME->parents = ['govuk','boost'];
 
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
