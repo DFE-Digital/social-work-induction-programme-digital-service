9069702edad8aa4c97ca9997e1f59eee1e766277
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-15 16:48:35 +0100

    Load govuk styles as child theme css post process

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
@@ -2,7 +2,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_govuk_swpdp';
-$plugin->version   = 2025090500;       
+$plugin->version   = 2025090501;       
 $plugin->requires  = 2023100900;       // 4.3+ (hooks available)
-$plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1';
