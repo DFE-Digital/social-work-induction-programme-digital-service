ea3d89b146f7874e3e2919897eab373c82c205dd
boblaw-dfe <armand.lawson@education.gov.uk>
2025-09-18 11:45:19 +0100

    SWIP-984 - Make notification service app host keys dependent on the module being run first

--- terraform/notification-service.tf
+++ terraform/notification-service.tf
@@ -42,6 +42,8 @@ module "notification-service" {
 data "azurerm_function_app_host_keys" "function_keys" {
   name                = "${var.resource_name_prefix}-fa-notification-service"
   resource_group_name = module.stack.resource_group_name
+
+  depends_on = [module.notification-service]
 }
 
 resource "azurerm_key_vault_secret" "function_key" {
