1b21643e06461fdf2e97f3c4593ea9f3d7f47ee9
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-24 15:04:30 +0100

    SWIP-1109 Hide groups dropdown for non staff users
    Hiding on the view database page. Refactored primary_nav to make the is_staff_like function a helper method.

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
@@ -1,15 +1,11 @@
 <?php
 
+use theme_govuk_swpdp\helpers\common_helpers as Helpers;
+use core\output\single_select;
+
 class theme_govuk_swpdp_core_renderer extends theme_govuk_core_renderer
 {
-    public function header(): string {
-        if ($this->page->pagetype === 'mod-data-view') {
-            $this->page->set_heading('');
-        }
-        return parent::header();
-    }    
-
-    /** Hide breadcrumbs on pages within a course */
+    /* Hide breadcrumbs on pages within a course */
     public function navbar(): string
     {
         if (
@@ -19,5 +15,33 @@ class theme_govuk_swpdp_core_renderer extends theme_govuk_core_renderer
             return '';
         }
         return parent::navbar();
+    }
+
+    /* Empty header on view database page to hide course name */
+    public function header(): string {
+        if ($this->page->pagetype === 'mod-data-view') {
+            $this->page->set_heading('');
+        }
+        return parent::header();
+    }    
+
+    /* Hide groups dropdown on database view page for non staff */
+    protected function render_single_select(single_select $select): string {
+        if (!$this->is_staff_viewing_database()) {
+            $data = $select->export_for_template($this);
+            if ($data->name === 'group') {
+                return '';
+            }
+        }
+        $data = $select->export_for_template($this);
+        return $this->render_from_template('core/single_select', $data);
+    }
+
+    private function is_staff_viewing_database(): bool {
+        if ($this->page->pagetype !== 'mod-data-view') {
+            return false;
+        }
+        global $USER;
+        return Helpers::is_staff_like($USER->id);
     }
 }
