f7ba6081eb264d94f41d532f60255b1ca4c21e70
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-24 11:41:38 +0100

    SWIP-1109 simplify empty heading logic
    And apply to the database page in general, as per designs

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
@@ -2,19 +2,12 @@
 
 class theme_govuk_swpdp_core_renderer extends theme_govuk_core_renderer
 {
-    public function context_header($headerinfo = null, $headinglevel = 1): string {
-        // Clear the heading on database activity page with no entries
+    public function header(): string {
         if ($this->page->pagetype === 'mod-data-view') {
-            try {
-                $manager = \mod_data\manager::create_from_coursemodule($this->page->cm);
-                if (!$manager->has_records()) {
-                    $this->page->set_heading('');
-                }
-            } catch (\Throwable $e) {}
+            $this->page->set_heading('');
         }
-
-        return parent::context_header($headerinfo, $headinglevel);
-    }
+        return parent::header();
+    }    
 
     /** Hide breadcrumbs on pages within a course */
     public function navbar(): string
