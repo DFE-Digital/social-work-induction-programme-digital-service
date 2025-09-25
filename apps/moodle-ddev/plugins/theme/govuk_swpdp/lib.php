545c732ae1a280a7e6eee23232c1b19df453eb75
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-25 15:25:48 +0100

    SWIP-1109 format empty db activity page (#255)
    * SWIP-1109 Empty h1 tag on db page with no entries
    
    This change is for all users and includes css overrides to ensure there is no white space with the empty h1. Could not remove the h1 entirely as mustache template is used on other pages that require the h1.
    
    * Move breadcrumb logic from govuk to child theme
    
    * SWIP-1109 simplify empty heading logic
    
    And apply to the database page in general, as per designs
    
    * SWIP-1109 Hide groups dropdown for non staff users
    
    Hiding on the view database page. Refactored primary_nav to make the is_staff_like function a helper method.
    
    * SWIP-1109 Change button name
    
    Language string file will need importing into each environment. We would ideally automate.
    
    * SWIP-1109 Remove elements that are not needed
    
    For the empty state of  database activity
    
    * SWIP-1109 Add style overrides via scss
    
    Use Moodle's scss compiler
    
    * SWIP-1109 Lib file comments
    
    * SWIP-1109 Button text change
    
    * SWIP-1109 Bump up theme version
    
    rebase main

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/lib.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/lib.php
@@ -33,3 +33,28 @@ function theme_govuk_swpdp_extend_settings_navigation($settingsnav, $context) {
     $url = new moodle_url('/theme/govuk_swpdp/course_portfolio_settings.php', ['id' => $courseid]);
     $settingsnav->add(get_string('myportfolio', 'theme_govuk_swpdp'), $url);
 }
+
+/**
+ * Main SCSS - add swpdp to govuk
+ */
+function theme_govuk_swpdp_get_main_scss_content($theme): string {
+    $parentscss = theme_govuk_get_main_scss_content($theme);
+    $themescss = @file_get_contents(__DIR__ . '/scss/govuk_swpdp.scss') ?: '';
+    return $parentscss . "\n" . $themescss;
+}
+
+/**
+ * SCSS to prepend
+ */
+function theme_govuk_swpdp_get_pre_scss($theme): string {
+    return theme_govuk_get_pre_scss($theme) . "\n" .
+           (@file_get_contents(__DIR__ . '/scss/pre.scss') ?: '');
+}
+
+/**
+ * SCSS to append (after main bundle)
+ */
+function theme_govuk_swpdp_get_extra_scss($theme): string {
+    return theme_govuk_get_extra_scss($theme) . "\n" .
+           (@file_get_contents(__DIR__ . '/scss/post.scss') ?: '');
+}
