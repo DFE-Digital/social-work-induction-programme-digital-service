c5927a104c82416ab2415c707cd56560e2d56c6d
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2025-09-26 16:36:19 +0100

    [SWIP] Reword buttons on mod/data/edit sticky footer (#256)
    * reword buttons on mod/data/edit sticky footer
    
    * Update theme_govuk_swpdp.php
    
    * Update renderers.php
    
    * Update renderers.php
    
    * Update renderers.php

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/lang/en/theme_govuk_swpdp.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/lang/en/theme_govuk_swpdp.php
@@ -17,6 +17,9 @@ $string['configtitle'] = 'GOVUK SWPDP theme settings';
 $string['enablelearnercustomnav'] = 'Enable custom navigation items for learners';
 $string['enablelearnercustomnav_desc'] = 'When enabled, learners see the custom navigation menu instead of the default.';
 
+$string['saveprogress'] = 'Save progress';
+$string['sendtoobserver'] = 'Send to observer';
+
 $string['settingssaved'] = 'Settings saved';
 $string['portfolioheading'] = 'Portfolio settings';
 $string['portfolioheading_desc'] = 'Configure where the My Portfolio nav item points.';
--- apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
@@ -62,4 +62,41 @@ class theme_govuk_swpdp_core_renderer extends theme_govuk_core_renderer
         global $USER;
         return Helpers::is_staff_like($USER->id);
     }
+
+    /**
+     * Override to change content used to render the sticky footer template
+     * on the add entry page for database activities (mod/data/edit.php)
+     *
+     * @param \core\output\sticky_footer $footer
+     * @return string
+     */
+    protected function render_sticky_footer(
+        \core\output\sticky_footer $footer,
+    ): string {
+        $data = $footer->export_for_template($this);
+
+        $isdataedit = $this->page->pagetype === 'mod-data-edit';
+        $rid = optional_param('rid', 0, PARAM_INT);
+        $isadd = $rid == 0;
+
+        if ($isdataedit && $isadd) {
+            $data['stickycontent'] = html_writer::empty_tag('input', [
+                'type' => 'submit',
+                'name' => 'saveandview',
+                'value' => get_string('saveprogress', 'theme_govuk_swpdp'),
+                'class' => 'govuk-button mx-1',
+            ]);
+            $data['stickycontent'] .= html_writer::link(
+                '',
+                get_string('sendtoobserver', 'theme_govuk_swpdp'),
+                ['class' => 'govuk-button govuk-button--secondary', 'role' => 'button'],
+            );
+        }
+
+        return $this->render_from_template('core/sticky_footer', $data);
+    }
+
+    public function firstview_fakeblocks(): string {
+        return '';
+    }
 }
