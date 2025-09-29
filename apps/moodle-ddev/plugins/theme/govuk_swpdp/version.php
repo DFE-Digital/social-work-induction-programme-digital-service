0961e8d485f6feca7efa5e9c8c4da33529b70f59
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-09-29 17:08:26 +0100

    SWIP-1109 Add heading styling for moodle empty database page (#259)
    * SWIP-1098 Fix method signature
    
    * SWIP-1109 Styles override for h2 to look like h1
    
    This should be a temporary measure. Also removing scss format as not needed for now.

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_govuk_swpdp';
-$plugin->version   = 2025092500;       
+$plugin->version   = 2025092900;       
 $plugin->requires  = 2023100900;       // 4.3+ (hooks available)
