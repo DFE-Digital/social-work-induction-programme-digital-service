3e5acaa50ae7c0beca5475f6465d7456583ce956
Cristina Tabacaru <163895464+cgtabacaru@users.noreply.github.com>
2025-10-01 11:57:08 +0100

    SWIP-1102 Remove moodle user tag link on database view entires pages (#266)
    * SWIP-1102 Convert user links to spans on db view
    
    * SWIP-1102 Remove user link styling

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_govuk_swpdp';
-$plugin->version   = 2025092900;       
+$plugin->version   = 2025093000;       
 $plugin->requires  = 2023100900;       // 4.3+ (hooks available)
