9f2f6da6697780b5bb2a0af919f44113547befae
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-10-01 15:12:52 +0100

    Update renderers.php

--- apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
+++ apps/moodle-ddev/plugins/theme/govuk_swpdp/renderers.php
@@ -85,8 +85,6 @@ class theme_govuk_swpdp_core_renderer extends theme_govuk_core_renderer
         $data = $footer->export_for_template($this);
 
         $isdataedit = $this->page->pagetype === 'mod-data-edit';
-        $rid = optional_param('rid', 0, PARAM_INT);
-        $isadd = $rid == 0;
 
         if ($isdataedit) {
             $data['stickycontent'] = html_writer::empty_tag('input', [
