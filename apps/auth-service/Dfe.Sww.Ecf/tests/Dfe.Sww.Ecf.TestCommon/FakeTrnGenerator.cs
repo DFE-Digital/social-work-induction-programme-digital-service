25e27b0e65b4a5e44009f75df45897237677884e
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-10-15 11:03:29 +0100

    Update FakeTrnGenerator.cs

--- apps/auth-service/Dfe.Sww.Ecf/tests/Dfe.Sww.Ecf.TestCommon/FakeTrnGenerator.cs
+++ apps/auth-service/Dfe.Sww.Ecf/tests/Dfe.Sww.Ecf.TestCommon/FakeTrnGenerator.cs
@@ -2,7 +2,7 @@ namespace Dfe.Sww.Ecf.TestCommon;
 
 public class FakeTrnGenerator
 {
-    private int _lastTrn = 4000000;
+    private int _lastTrn = 40000000;
 
     public string GenerateTrn() => Interlocked.Increment(ref _lastTrn).ToString();
 }
