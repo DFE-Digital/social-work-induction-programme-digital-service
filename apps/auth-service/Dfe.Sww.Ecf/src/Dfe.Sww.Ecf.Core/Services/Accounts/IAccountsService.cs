f7d1201b8dd8215918c5245dd92b224b4eab173d
Sam Nicholson <161619204+SamNich-Hippo@users.noreply.github.com>
2025-10-24 13:41:24 +0100

    [SWIP-1052] Add get by swe id endpoint (#310)
    * add get by swe id endpoint
    
    * Update AccountsController.cs

--- apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/Services/Accounts/IAccountsService.cs
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/Services/Accounts/IAccountsService.cs
@@ -13,4 +13,5 @@ public interface IAccountsService
     Task<PersonDto?> UpdateAsync(Person person);
     Task<PersonDto?> DeleteAsync(Guid id);
     Task<PersonDto?> GetByEmailAsync(string email);
+    Task<PersonDto?> GetBySocialWorkEnglandNumberAsync(string socialWorkerEnglandNumber);
 }
