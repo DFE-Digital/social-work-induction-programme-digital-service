c211cadb80e123bef33c610eba8f966b68fd565d
Sam Nicholson <sam.nicholson@hippodigital.co.uk>
2025-11-19 10:32:47 +0000

    remove required external org it

--- apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Mappings/OrganisationMapping.cs
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Mappings/OrganisationMapping.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Dfe.Sww.Ecf.Core.DataStore.Postgres.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Dfe.Sww.Ecf.Core.DataStore.Postgres.Mappings;
 
@@ -10,8 +10,6 @@ public class OrganisationMapping : IEntityTypeConfiguration<Organisation>
         builder.ToTable("organisations");
         builder.HasKey(o => o.OrganisationId);
         builder.Property(o => o.OrganisationId).HasDefaultValueSql("gen_random_uuid()");
-        builder.HasIndex(o => o.ExternalOrganisationId).HasDatabaseName(Organisation.ExternalOrganisationIdIndexName);
-        builder.Property(o => o.ExternalOrganisationId).IsRequired();
         builder.Property(o => o.OrganisationName).HasMaxLength(255).IsRequired();
         builder.Property(o => o.CreatedOn).HasDefaultValueSql("now()");
         builder.Property(o => o.Region).HasMaxLength(255);
---
+++ apps/auth-service/Dfe.Sww.Ecf/src/Dfe.Sww.Ecf.Core/DataStore/Postgres/Migrations/20251119102624_RemoveExternalOrgIdRequired.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dfe.Sww.Ecf.Core.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExternalOrgIdRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_organisation_external_organisation_id",
+                table: "organisations");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "external_organisation_id",
+                table: "organisations",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "external_organisation_id",
+                table: "organisations",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_organisation_external_organisation_id",
+                table: "organisations",
+                column: "external_organisation_id");
+        }
+    }
+}
